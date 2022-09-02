f3f32deddcc7efb7d69a4bbd62ae64d9fce5dc98
Bruna Yumi Nagahashi <bruna.nagahashi@gmail.com>
2022-09-01 22:57:08 -0300

    change js code

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 const calculator = document.querySelector('.calculator');
 const keys = calculator.querySelector('.calculator-keys');
 const display = document.querySelector('.calculator-display');
-
-const previousKeyType = calculator.dataset;
-
+let previousKeyType = calculator.dataset.previousKeyType;
+let firstValue = calculator.dataset.firstValue;
+let operator = ''
+let secondValue = calculator.dataset.secondValue;
+let previousKey = calculator.dataset.previousKey;
 keys.addEventListener('click', (e) => {
     if (e.target.matches('button')) {
         const key = e.target;
         const action = key.dataset.action;
         const keyContent = key.textContent;
         const displayNum = display.textContent;
+        
         if (!action) {
-            console.log('number key');
+            previousKey = 'number'
+            if (!displayNum.includes('.')) {
+                display.textContent = displayNum + '.'
+            }
+           
             if(displayNum === '0' || previousKeyType === 'operator'){
+                previousKeyType = ''
                 display.textContent = keyContent;
+                console.log(previousKeyType)
             } else {
                 display.textContent = displayNum + keyContent
             }
@@ -25,18 +34,51 @@ keys.addEventListener('click', (e) => {
             action === 'multiply' ||
             action === 'divide'
         ) {
-           key.classList.toggle('is-depressed');
-           previousKeyType = 'operator'
+            
+            previousKeyType = 'operator' 
+            firstValue = displayNum
+            operator = action 
+            console.log(firstValue)
+            console.log(operator)
         }
 
         if (action === 'decimal') {
-            display.textContent = displayNum + '.'
+            if (!displayNum.includes('.')) {
+                display.textContent = displayNum + '.'
+              } else if (previousKeyType === 'operator') {
+                display.textContent = '0.'
+              }
+            previousKey = 'decimal'
         }
         if (action === 'clear') {
-            console.log('clear key');
+            previousKey = 'clear'
         }
         if (action === 'calculate') {
-            console.log('equal key!');
+            secondValue = displayNum;
+            previousKey = 'calculate';
+
+            if(firstValue && operator){
+                display.textContent = calculate(firstValue, operator, secondValue);
+            }
+            
+            firstValue = displayNum;
+            previousKeyType = 'operator';
+            operator = action;
         }
     }
 });
+const calculate = (n1, operator, n2) => {
+    let result = '';
+    if(operator === 'add'){
+        result = parseFloat(n1) + parseFloat(n2);
+
+    } else if (operator === 'subtract') {
+        result = parseFloat(n1) - parseFloat(n2);
+    }else if (operator === 'multiply'){
+        result = parseFloat(n1)*parseFloat(n2);
+    } else if (operator === 'divide'){
+        result = parseFloat(n1)/parseFloat(n2);
+    }
+
+    return result;
+}
